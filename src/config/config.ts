b4f040a91bbdacb29b6bbb48ac9cc94aa5122910
yoyofx <zl.hxd@hotmail.com>
2021-09-27 14:48:18 +0800

    动态路由

--- src/config/config.ts
+++ src/config/config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   layout: {
     // https://umijs.org/zh-CN/plugins/plugin-layout
-    locale: false,
+    locale: true,
     siderWidth: 208,
     ...defaultSettings,
   },
@@ -32,288 +32,99 @@ export default defineConfig({
     ie: 11,
   },
   // umi routes: https://umijs.org/docs/routing
-  // routes: [
-  //   {
-  //     path: '/user',
-  //     layout: false,
-  //     routes: [
-  //       {
-  //         path: '/user/login',
-  //         layout: false,
-  //         name: 'login',
-  //         component: './user/Login',
-  //       },
-  //       {
-  //         path: '/user',
-  //         redirect: '/user/login',
-  //       },
-  //       {
-  //         name: 'register-result',
-  //         icon: 'smile',
-  //         path: '/user/register-result',
-  //         component: './user/register-result',
-  //       },
-  //       {
-  //         name: 'register',
-  //         icon: 'smile',
-  //         path: '/user/register',
-  //         component: './user/register',
-  //       },
-  //       {
-  //         component: '404',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     path: '/dashboard',
-  //     name: '仪表盘',
-  //     icon: 'dashboard',
-  //     routes: [
-  //       {
-  //         path: '/dashboard',
-  //         redirect: '/dashboard/analysis',
-  //       },
-  //       {
-  //         name: 'analysis',
-  //         icon: 'smile',
-  //         path: '/dashboard/analysis',
-  //         component: './dashboard/analysis',
-  //       },
-  //       {
-  //         name: 'monitor',
-  //         icon: 'smile',
-  //         path: '/dashboard/monitor',
-  //         component: './dashboard/monitor',
-  //       },
-  //       {
-  //         name: 'workplace',
-  //         icon: 'smile',
-  //         path: '/dashboard/workplace',
-  //         component: './dashboard/workplace',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     path: '/form',
-  //     icon: 'form',
-  //     name: 'form',
-  //     routes: [
-  //       {
-  //         path: '/form',
-  //         redirect: '/form/basic-form',
-  //       },
-  //       {
-  //         name: 'basic-form',
-  //         icon: 'smile',
-  //         path: '/form/basic-form',
-  //         component: './form/basic-form',
-  //       },
-  //       {
-  //         name: 'step-form',
-  //         icon: 'smile',
-  //         path: '/form/step-form',
-  //         component: './form/step-form',
-  //       },
-  //       {
-  //         name: 'advanced-form',
-  //         icon: 'smile',
-  //         path: '/form/advanced-form',
-  //         component: './form/advanced-form',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     path: '/list',
-  //     icon: 'table',
-  //     name: 'list',
-  //     routes: [
-  //       {
-  //         path: '/list/search',
-  //         name: 'search-list',
-  //         component: './list/search',
-  //         routes: [
-  //           {
-  //             path: '/list/search',
-  //             redirect: '/list/search/articles',
-  //           },
-  //           {
-  //             name: 'articles',
-  //             icon: 'smile',
-  //             path: '/list/search/articles',
-  //             component: './list/search/articles',
-  //           },
-  //           {
-  //             name: 'projects',
-  //             icon: 'smile',
-  //             path: '/list/search/projects',
-  //             component: './list/search/projects',
-  //           },
-  //           {
-  //             name: 'applications',
-  //             icon: 'smile',
-  //             path: '/list/search/applications',
-  //             component: './list/search/applications',
-  //           },
-  //         ],
-  //       },
-  //       {
-  //         path: '/list',
-  //         redirect: '/list/table-list',
-  //       },
-  //       {
-  //         name: 'table-list',
-  //         icon: 'smile',
-  //         path: '/list/table-list',
-  //         component: './list/table-list',
-  //       },
-  //       {
-  //         name: 'basic-list',
-  //         icon: 'smile',
-  //         path: '/list/basic-list',
-  //         component: './list/basic-list',
-  //       },
-  //       {
-  //         name: 'card-list',
-  //         icon: 'smile',
-  //         path: '/list/card-list',
-  //         component: './list/card-list',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     path: '/profile',
-  //     name: 'profile',
-  //     icon: 'profile',
-  //     routes: [
-  //       {
-  //         path: '/profile',
-  //         redirect: '/profile/basic',
-  //       },
-  //       {
-  //         name: 'basic',
-  //         icon: 'smile',
-  //         path: '/profile/basic',
-  //         component: './profile/basic',
-  //       },
-  //       {
-  //         name: 'advanced',
-  //         icon: 'smile',
-  //         path: '/profile/advanced',
-  //         component: './profile/advanced',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     name: 'result',
-  //     icon: 'CheckCircleOutlined',
-  //     path: '/result',
-  //     routes: [
-  //       {
-  //         path: '/result',
-  //         redirect: '/result/success',
-  //       },
-  //       {
-  //         name: 'success',
-  //         icon: 'smile',
-  //         path: '/result/success',
-  //         component: './result/success',
-  //       },
-  //       {
-  //         name: 'fail',
-  //         icon: 'smile',
-  //         path: '/result/fail',
-  //         component: './result/fail',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     name: 'exception',
-  //     icon: 'warning',
-  //     path: '/exception',
-  //     routes: [
-  //       {
-  //         path: '/exception',
-  //         redirect: '/exception/403',
-  //       },
-  //       {
-  //         name: '403',
-  //         icon: 'smile',
-  //         path: '/exception/403',
-  //         component: './exception/403',
-  //       },
-  //       {
-  //         name: '404',
-  //         icon: 'smile',
-  //         path: '/exception/404',
-  //         component: './exception/404',
-  //       },
-  //       {
-  //         name: '500',
-  //         icon: 'smile',
-  //         path: '/exception/500',
-  //         component: './exception/500',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     name: 'account',
-  //     icon: 'user',
-  //     path: '/account',
-  //     routes: [
-  //       {
-  //         "component": "./account/manage",
-  //         "name": "manage",
-  //         "path": "/account/manage"
-  //       },
-  //       {
-  //         "component": "./account/role",
-  //         "name": "role",
-  //         "path": "/account/role"
-  //       },
-  //       {
-  //         "component": "./account/tenant",
-  //         "name": "tenant",
-  //         "path": "/account/tenant"
-  //       }
-  //     ],
-  //   },
-  //   {
-  //     name: 'editor',
-  //     icon: 'highlight',
-  //     path: '/editor',
-  //     routes: [
-  //       {
-  //         path: '/editor',
-  //         redirect: '/editor/flow',
-  //       },
-  //       {
-  //         name: 'flow',
-  //         icon: 'smile',
-  //         path: '/editor/flow',
-  //         component: './editor/flow',
-  //       },
-  //       {
-  //         name: 'mind',
-  //         icon: 'smile',
-  //         path: '/editor/mind',
-  //         component: './editor/mind',
-  //       },
-  //       {
-  //         name: 'koni',
-  //         icon: 'smile',
-  //         path: '/editor/koni',
-  //         component: './editor/koni',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     path: '/',
-  //     redirect: '/dashboard/analysis',
-  //   },
-  //   {
-  //     component: '404',
-  //   },
-  // ],
+  routes: [
+    {
+      path: '/dashboard',
+      icon: 'dashboard',
+      routes: [
+        {
+          name: 'analysis',
+          icon: 'smile',
+          path: '/dashboard/analysis',
+          component: './dashboard/analysis',
+        }
+      ]
+    },
+    {
+      path: '/user',
+      layout: false,
+      routes: [
+        {
+          path: '/user/login',
+          layout: false,
+          name: 'login',
+          component: './user/Login',
+        },
+        {
+          path: '/user',
+          redirect: '/user/login',
+        },
+        {
+          name: 'register',
+          icon: 'smile',
+          path: '/user/register',
+          component: './user/register',
+        },
+      ],
+    },
+    {
+      path: '/exception',
+      layout: false,
+      routes: [
+        {
+          path: '/exception',
+          redirect: '/exception/403',
+        },
+        {
+          name: '403',
+          icon: 'smile',
+          path: '/exception/403',
+          component: './exception/403',
+        },
+        {
+          name: '404',
+          icon: 'smile',
+          path: '/exception/404',
+          component: './exception/404',
+        },
+        {
+          name: '500',
+          icon: 'smile',
+          path: '/exception/500',
+          component: './exception/500',
+        },
+      ],
+    },
+    {
+      name: 'account',
+      icon: 'user',
+      path: '/account',
+      routes: [
+        {
+          "component": "./account/manage",
+          "name": "manage",
+          "path": "/account/manage"
+        },
+        {
+          "component": "./account/role",
+          "name": "role",
+          "path": "/account/role"
+        },
+        {
+          "component": "./account/tenant",
+          "name": "tenant",
+          "path": "/account/tenant"
+        }
+      ],
+    },
+    {
+      path: '/',
+      redirect: '/dashboard/analysis',
+    },
+    {
+      component: '404',
+    },
+  ],
   // Theme for antd: https://ant.design/docs/react/customize-theme-cn
   theme: {
     'primary-color': defaultSettings.primaryColor,
