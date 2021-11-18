96901fe41c47d684a2a32633447bf5bbcde576e4
Cui <291527070@qq.com>
2021-11-18 14:18:24 +0800

    添加集群导入

--- src/src/pages/resources/clusters/cluster_service.ts
+++ src/src/pages/resources/clusters/cluster_service.ts
@@ -6,13 +6,29 @@ export async function getClusterList(params: {
     pageIndex?: number;
     pageSize?: number;
     current?: number;
-},sort:Record<string, any>,filter:any) {
-    let resData=await request< ApiResponse<ClusterItem>>("/v1/cluster/list",{
-        method:'GET',
-        params:params
+}, sort: Record<string, any>, filter: any) {
+    let resData = await request<ApiResponse<ClusterItem>>("/v1/cluster/list", {
+        method: 'GET',
+        params: params
     })
-    return new Promise<any>(x=>{
+    return new Promise<any>(x => {
         x(resData)
     })
-    
+}
+
+export async function importConfigFile(file: any){
+    const formData = new FormData()
+    formData.append('file1',file)
+    let resData = await request<ApiResponse<boolean>>("/v1/cluster/clusterByConfig", {
+        method: 'POST',
+        body:formData
+    })
+    return resData
+}
+
+export async function removeCluster(id:number){
+    let resData=await request<ApiResponse<boolean>>(`/v1/cluster/delclusterinfo?id=${id}`, {
+        method: 'DELETE',
+    })
+    return resData
 }
