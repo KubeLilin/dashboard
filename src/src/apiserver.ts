f6d99045a5713dcc59ec436a051f78478d93e325
yoyofx <zl.hxd@hotmail.com>
2022-01-17 10:10:07 +0800

    1

--- src/src/apiserver.ts
+++ src/src/apiserver.ts
@@ -3,6 +3,6 @@ const isDev = process.env.NODE_ENV === 'development';
 
 
 export const API_SERVER = {
-   api: isDev? 'http://localhost:8080': 'http://api.yoyogo.run:30980/sgr', 
-   ws:  isDev? 'ws://localhost:8080'   : 'ws://api.yoyogo.run:30980/sgr'
+   api: isDev? 'http://api.yoyogo.run:30980/sgr': 'http://api.yoyogo.run:30980/sgr', 
+   ws:  isDev? 'ws://api.yoyogo.run:30980/sgr'   : 'ws://api.yoyogo.run:30980/sgr'
 }
