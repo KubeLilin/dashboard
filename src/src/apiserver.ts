07673796559ba622612f50a925c416e4271769a3
yoyofx <zl.hxd@hotmail.com>
2022-03-18 00:03:23 +0800

    fixed release websocket bugs

--- src/src/apiserver.ts
+++ src/src/apiserver.ts
@@ -4,5 +4,5 @@ const isDev = process.env.NODE_ENV === 'development';
 
 export const API_SERVER = {
    api: isDev? 'http://localhost:8080': 'http://kubelilin-api.yoyogo.run', 
-   ws:  isDev? 'ws://localhost:8080'   : 'http://kubelilin-api.yoyogo.run'
+   ws:  isDev? 'ws://localhost:8080'   : 'ws://kubelilin-api.yoyogo.run'
 }
