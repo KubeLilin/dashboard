48a3a0b4c54ebad21f0954e7d7a4bcc352dca4ae
Cui.Jiaqing <291527070@qq.com>
2022-10-14 09:00:03 +0800

    probe

--- src/src/pages/applications/probe/index.tsx
+++ src/src/pages/applications/probe/index.tsx
@@ -1,5 +1,5 @@
-import ProForm, { ModalForm, ProFormGroup, ProFormInstance, ProFormSelect, ProFormText } from '@ant-design/pro-form';
-import { Checkbox, InputNumber, Switch } from 'antd';
+import ProForm, { DrawerForm, ModalForm, ProFormGroup, ProFormInstance, ProFormSelect, ProFormText } from '@ant-design/pro-form';
+import { Checkbox, Divider, InputNumber, Switch } from 'antd';
 import Form from 'antd/lib/form';
 import React, { SetStateAction, useState, Dispatch, useEffect, useRef, } from 'react';
 import { ProbeFormData } from './probe_data';
@@ -13,58 +13,69 @@ export interface Props {
 }
 
 const Probe: React.FC<Props> = (props: Props) => {
+    const [livenessRequire, livenessRequireHandler] = useState<boolean>();
+    const [readnessRequire, readnessRequireHandler] = useState<boolean>();
     const actionRef = useRef<ProFormInstance>();
     const [form] = Form.useForm();
-    return (<ModalForm<ProbeFormData> title="POD探针设置" visible={props.visibleFunc[0]} form={form} formRef={actionRef} onVisibleChange={(visible) => {
-        const setVisible = props.visibleFunc[1]
-        setVisible?.(visible)
-        if (visible) {
+    return (<DrawerForm<ProbeFormData> title="POD探针设置"
+        width={500}
+        visible={props.visibleFunc[0]}
+        form={form} formRef={actionRef}
+        onVisibleChange={(visible) => {
+            const setVisible = props.visibleFunc[1]
+            setVisible?.(visible)
+            if (visible) {
 
-        }
-    }} >
-        <ProFormGroup title='Liveness'>
-            <ProForm.Item name='enableLiveness' label='开启Liveness探针'>
-            <Switch checkedChildren="开启" unCheckedChildren="关闭" defaultChecked />
+            }
+        }} >
+        <ProFormGroup label='Liveness探针配置'>
+            <ProForm.Item name='enableLiveness' >
+                <Switch checkedChildren="开启" unCheckedChildren="关闭" defaultChecked onChange={(checked: boolean) => {
+                    livenessRequireHandler(checked)
+                }} />
             </ProForm.Item>
-            <ProForm.Item name='livenessport' label='请求端口'>
+            <ProForm.Item name='livenessurl' label='请求路径' required={livenessRequire}>
+                <ProFormText width={400}></ProFormText>
+            </ProForm.Item>
+            <ProForm.Item name='livenessport' label='请求端口' required={livenessRequire}>
                 <InputNumber min={0}></InputNumber>
             </ProForm.Item>
-            <ProForm.Item name='livenessurl' label='请求路径'>
-                <ProFormText></ProFormText>
-            </ProForm.Item>
-            <ProForm.Item name='livenessInitialDelaySeconds' label='首次执行延迟秒数'>
+            <ProForm.Item name='livenessInitialDelaySeconds' label='首次执行延迟秒数' required={livenessRequire}>
                 <InputNumber min={0}></InputNumber>
             </ProForm.Item>
-            <ProForm.Item name='livenessPeriodSeconds' label='检查间隔秒数'>
+            <ProForm.Item name='livenessPeriodSeconds' label='检查间隔秒数' required={livenessRequire}>
                 <InputNumber min={0}></InputNumber>
             </ProForm.Item>
-            <ProFormSelect name='livenessreqScheme' label='请求协议' options={[
+            <ProFormSelect name='livenessreqScheme' label='请求协议' width={400} options={[
                 { label: 'HTTP', value: 'HTTP' }
-            ]}>
+            ]} required={livenessRequire}>
             </ProFormSelect>
         </ProFormGroup>
-        <ProFormGroup title='Readiness'>
-            <ProForm.Item name='enableReadiness' label='开启Readiness探针'>
-            <Switch checkedChildren="开启" unCheckedChildren="关闭" defaultChecked />
+        <Divider />
+        <ProFormGroup label='Readiness探针配置'>
+            <ProForm.Item name='enableReadiness' label=''>
+                <Switch checkedChildren="开启" unCheckedChildren="关闭" defaultChecked onChange={(checked: boolean) => {
+                    readnessRequireHandler(checked)
+                }} />
             </ProForm.Item>
-            <ProForm.Item name='readinessPort' label='请求端口'>
+            <ProForm.Item name='readinessUrl' label='请求路径' required={readnessRequire}>
+                <ProFormText width={400}></ProFormText>
+            </ProForm.Item>
+            <ProForm.Item name='readinessPort' label='请求端口' required={readnessRequire}>
                 <InputNumber min={0}></InputNumber>
             </ProForm.Item>
-            <ProForm.Item name='readinessUrl' label='请求路径'>
-                <ProFormText></ProFormText>
-            </ProForm.Item>
-            <ProForm.Item name='readinessInitialDelaySeconds' label='首次执行延迟秒数'>
+            <ProForm.Item name='readinessInitialDelaySeconds' label='首次执行延迟秒数' required={readnessRequire}>
                 <InputNumber min={0}></InputNumber>
             </ProForm.Item>
-            <ProForm.Item name='readinessPeriodSeconds' label='检查间隔秒数'>
+            <ProForm.Item name='readinessPeriodSeconds' label='检查间隔秒数' required={readnessRequire}>
                 <InputNumber min={0}></InputNumber>
             </ProForm.Item>
-            <ProFormSelect name='readinessReqScheme' label='请求协议' options={[
+            <ProFormSelect name='readinessReqScheme' label='请求协议' width={400} options={[
                 { label: 'HTTP', value: 'HTTP' }
-            ]}>
+            ]} required={readnessRequire}>
             </ProFormSelect>
         </ProFormGroup>
-    </ModalForm>);
+    </DrawerForm>);
 }
 
 export default Probe;
