82a95b306e84ae2c611482ef503ac20ce9c3e549
Cui.Jiaqing <291527070@qq.com>
2022-12-13 16:43:31 +0800

    probe

--- src/src/pages/applications/advancedDeployment/forms/probe/probe_data.ts
+++ src/src/pages/applications/advancedDeployment/forms/probe/probe_data.ts
@@ -1,43 +1,43 @@
-export type ProbeData={
-    dpId:number,
-    enableReadiness:boolean,
-    enableLiveness:boolean
-    readiness:ProbeItem,
-    liveness:ProbeItem
+export type ProbeData = {
+    dpId: number,
+    enableReadiness: boolean,
+    enableLiveness: boolean
+    readiness: ProbeItem,
+    liveness: ProbeItem
 }
 
-export type ProbeItem={
-    type:string,
-    port:number,
-    url:string,
-    reqScheme:string
+export type ProbeItem = {
+    type: string,
+    port: number,
+    url: string,
+    reqScheme: string
 }
 
-export type ProbeFormData={
-    dpId:number,
+export type ProbeFormData = {
+    dpId: number,
 
-    enableLifecycle:boolean,
-    enableReadiness:boolean,
-    enableLiveness:boolean,
+    enableLifecycle: boolean,
+    enableReadiness: boolean,
+    enableLiveness: boolean,
 
-    terminationGracePeriodSeconds:number, // 定义在 deployment资源中
-    lifecyclePreStop:string,        // 定义在容器中
-    lifecyclePreStart:string,       // 
-    
-    readinessType:string,           // 定义在容器中
-    readinessPort:number,
-    readinessUrl:string,
-    readinessReqScheme:string
-    readinessInitialDelaySeconds:number,
-    readinessTimeoutSeconds:number,
-    readinessPeriodSeconds:number
+    terminationGracePeriodSeconds: number, // 定义在 deployment资源中
+    lifecyclePreStop: string,        // 定义在容器中
+    lifecyclePreStart: string,       // 
 
-    livenessType:string,            //定义在容器中
-    livenessPort:number,
-    livenessUrl:string,
-    livenessReqScheme:string,
-    livenessInitialDelaySeconds:number,
-    livenessTimeoutSeconds:number,
-    livenessPeriodSeconds:number
+    readinessType: string,           // 定义在容器中
+    readinessPort: number,
+    readinessUrl: string,
+    readinessReqScheme: string
+    readinessInitialDelaySeconds: number,
+    readinessTimeoutSeconds: number,
+    readinessPeriodSeconds: number
+
+    livenessType: string,            //定义在容器中
+    livenessPort: number,
+    livenessUrl: string,
+    livenessReqScheme: string,
+    livenessInitialDelaySeconds: number,
+    livenessTimeoutSeconds: number,
+    livenessPeriodSeconds: number
 
 }
